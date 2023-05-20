e969ec3fa71c623c5e0ef1f33e08b99ee1a9ea99
Lasse <gruenhagen.lasse@gmail.com>
2023-05-20 13:11:52 +0200

    heroku

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask import render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
 db = SQLAlchemy()
 from models import Task
 from forms import TaskForm
