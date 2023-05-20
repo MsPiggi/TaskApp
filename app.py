47bccd8f6dfe89b6fd7d70086b302c3cab19e9f5
Lasse <gruenhagen.lasse@gmail.com>
2023-05-20 18:01:17 +0200

    go

--- app.py
+++ app.py
@@ -1,82 +1,71 @@
 import os
-from flask import Flask
-from flask import render_template, request, jsonify, redirect, url_for
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy
-
-
-app = Flask(__name__)
-if 'DATABASE_URL' in os.environ:
-    # Heroku database URL
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yjtslemzwfespj:ef2c9832eebf12f4d8e0c264c677d2034c435dd3f27ecc6ca6b43426a73750cb@ec2-34-197-91-131.compute-1.amazonaws.com:5432/d28gb5rnh8v90u' #os.environ['DATABASE_URL']
-else:
-    # Local database URL
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/example'
-
-
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yjtslemzwfespj:ef2c9832eebf12f4d8e0c264c677d2034c435dd3f27ecc6ca6b43426a73750cb@ec2-34-197-91-131.compute-1.amazonaws.com:5432/d28gb5rnh8v90u'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/example'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['DEBUG'] = True  # Enable/Disable debugging
-
-
-db = SQLAlchemy()
-db.init_app(app)
-from models import Task
+from models import setup_db, db, Task 
 from forms import TaskForm
 
+def create_app():
+    app = Flask(__name__)
+    setup_db(app)
+
+    """def create_database():
+        with app.app_context():
+            db.create_all()
+            print("Database created successfully!") """
 
 
-def create_database():
-    with app.app_context():
-      db.create_all()
-      print("Database created successfully!")
+
+    @app.route('/')
+    def index():
+        tasks = Task.query.all()
+
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            return jsonify(tasks)
+
+        return render_template('index.html') 
 
 
-@app.route('/')
-def index():
-  tasks = Task.query.all()
+    @app.route('/create', methods=['POST'])
+    def create_task():
+        user_input = request.get_json()
+        form = TaskForm(data=user_input)
+        
+        # Validate the form
+        if form.validate():
+            task = Task(title=form.title.data)
+            db.session.add(task)
+            db.session.commit()
+            return jsonify(task)
+        
+        return redirect(url_for('index'))
 
-  if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-      return jsonify(tasks)
 
-  return render_template('index.html') 
+    @app.route('/delete', methods=['POST'])
+    def delete_task():
+        task_id = request.get_json().get('id')
+        task = Task.query.filter_by(id=task_id).first()
+        db.session.delete(task)
+        db.session.commit()
+        return jsonify({'result':'OK'}),200
 
 
-@app.route('/create', methods=['POST'])
-def create_task():
-    user_input = request.get_json()
-    form = TaskForm(data=user_input)
+    @app.route('/complete', methods=['POST'])
+    def complete_task():
+        task_id = request.get_json().get('id')
+        task = Task.query.filter_by(id=task_id).first()
+        task.completed = True
+        db.session.add(task)
+        db.session.commit()
+        return jsonify({'result':'OK'}),200 
     
-    # Validate the form
-    if form.validate():
-      task = Task(title=form.title.data)
-      db.session.add(task)
-      db.session.commit()
-      return jsonify(task)
-    
-    return redirect(url_for('index'))
+    return app
 
 
-@app.route('/delete', methods=['POST'])
-def delete_task():
-    task_id = request.get_json().get('id')
-    task = Task.query.filter_by(id=task_id).first()
-    db.session.delete(task)
-    db.session.commit()
-    return jsonify({'result':'OK'}),200
-
-
-@app.route('/complete', methods=['POST'])
-def complete_task():
-    task_id = request.get_json().get('id')
-    task = Task.query.filter_by(id=task_id).first()
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
-    return jsonify({'result':'OK'}),200 
-
+app = create_app()
 
 if __name__ == '__main__':
-  create_database()
-  app.run()
+    app.run()
+
+
+  
--- models.py
+++ models.py
@@ -1,9 +1,44 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
 from dataclasses import dataclass
 from datetime import datetime
-from app import db
 
 
+db = SQLAlchemy()
+
+def setup_db(app):
+    if 'DATABASE_URL' in os.environ:
+        # Heroku database URL
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yjtslemzwfespj:ef2c9832eebf12f4d8e0c264c677d2034c435dd3f27ecc6ca6b43426a73750cb@ec2-34-197-91-131.compute-1.amazonaws.com:5432/d28gb5rnh8v90u' #os.environ['DATABASE_URL']
+    else:
+        # Local database URL
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/example'
+
+
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yjtslemzwfespj:ef2c9832eebf12f4d8e0c264c677d2034c435dd3f27ecc6ca6b43426a73750cb@ec2-34-197-91-131.compute-1.amazonaws.com:5432/d28gb5rnh8v90u'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/example'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['DEBUG'] = True  # Enable/Disable debugging
+    db.app = app
+    db.init_app(app)
+    db_drop_and_create(app)
+    
+
+def db_drop_and_create(app):
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        insert_basic_data()
+
+
+def insert_basic_data():
+    task1 = Task(title="Task 1", date=datetime.now(), completed=False)
+    db.session.add(task1)
+    
+    task2 = Task(title="Task 2", date=datetime.now(), completed=True)
+    db.session.add(task2)
+    
+    db.session.commit()
 
 
 
